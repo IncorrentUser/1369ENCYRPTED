f0dfaa9dadf138f06836507c00bd81c8356c6133
IncorrentUser <burakyydk@gmail.com>
2025-01-12 03:49:35 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,37 @@
-wait(5)
-player:Kick("You've Banned From This Experience Reason: Expo")
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+
+local knownPlayers = {"ArchieN37", "Zonderhize", "PassedForever"}
+
+local toolGiverPosition = Vector3.new(-601.95, 273.094, 837.366)
+local toolReceiverPosition = Vector3.new(-432.002, 287.344, 214.992)
+
+local function checkNewPlayer()
+    for _, newPlayer in pairs(Players:GetPlayers()) do
+        if not table.find(knownPlayers, newPlayer.Name) then
+            if newPlayer.Character then
+                newPlayer.Character:BreakJoints() 
+            end
+        end
+    end
+end
+
+while true do
+    checkNewPlayer()  -- Yeni oyuncuları kontrol et
+
+    teleportTo(toolGiverPosition)
+    wait(0.5)
+
+    teleportTo(toolReceiverPosition)
+    wait(0.5)
+end
+
+local function teleportTo(position)
+    if character and character.PrimaryPart then
+        character:SetPrimaryPartCFrame(CFrame.new(position))
+        wait(0.5)
+    else
+        warn("yapamadın ki baba olmadı iste")
+    end
+end
