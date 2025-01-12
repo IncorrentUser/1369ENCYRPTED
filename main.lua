f153e53bda6fa4dae9d561f51b118fed37701f46
IncorrentUser <burakyydk@gmail.com>
2025-01-12 03:34:05 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,6 +2,17 @@ local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 
+    wait(2)
+    if #Players:GetPlayers() > 2 then
+        local thirdPlayer = Players:GetPlayers()[3]
+        if thirdPlayer and thirdPlayer.Name ~= "ArchieN37" and thirdPlayer.Name ~= "Zonderhize" then
+            print("Oyuncu sayısı 3. Oyuncu atılıyor.")
+            player:Kick("You've Banned From This Experience Reason: Expo")
+            break
+        end
+    end
+end
+
 local toolGiverPosition = Vector3.new(-601.95, 273.094, 837.366)
 local toolReceiverPosition = Vector3.new(-432.002, 287.344, 214.992)
 
@@ -32,4 +43,14 @@ while true do
 
     teleportTo(toolReceiverPosition)
     wait(0.5)
+end
+    wait(2)
+    if #Players:GetPlayers() > 2 then
+        local thirdPlayer = Players:GetPlayers()[3]
+        if thirdPlayer and thirdPlayer.Name ~= "ArchieN37" and thirdPlayer.Name ~= "Zonderhize" then
+            print("Oyuncu sayısı 3. Oyuncu atılıyor.")
+            player:Kick("You've Banned From This Experience Reason: Expo")
+            break
+        end
+    end
 end
