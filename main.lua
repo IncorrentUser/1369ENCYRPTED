e80c17b0cde9dba8e61fe1db1fd9b6280da07321
IncorrentUser <burakyydk@gmail.com>
2025-01-12 03:53:39 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,36 +2,36 @@ local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 
-local knownPlayers = {"ArchieN37", "Zonderhize", "PassedForever"}
+local knownPlayers = {"ArchieN37", "Zonderhize", PassedForever"}
 
 local toolGiverPosition = Vector3.new(-601.95, 273.094, 837.366)
 local toolReceiverPosition = Vector3.new(-432.002, 287.344, 214.992)
 
-local function checkNewPlayer()
-    for _, newPlayer in pairs(Players:GetPlayers()) do
-        if not table.find(knownPlayers, newPlayer.Name) then
-            if newPlayer.Character then
-                newPlayer.Character:BreakJoints() 
-            end
+local function handlePlayerJoin(newPlayer)
+    if table.find(knownPlayers, newPlayer.Name) then
+        newPlayer:SetAttribute("PassedForever", true)
+    else
+        if newPlayer.Character then
+            newPlayer.Character:BreakJoints()
         end
     end
 end
 
-while true do
-    checkNewPlayer()  -- Yeni oyuncuları kontrol et
-
-    teleportTo(toolGiverPosition)
-    wait(0.5)
-
-    teleportTo(toolReceiverPosition)
-    wait(0.5)
-end
+Players.PlayerAdded:Connect(handlePlayerJoin)
 
 local function teleportTo(position)
     if character and character.PrimaryPart then
         character:SetPrimaryPartCFrame(CFrame.new(position))
         wait(0.5)
     else
-        warn("yapamadın ki baba olmadı iste")
+        warn("yanlıs")
     end
+end
+
+while true do
+    teleportTo(toolGiverPosition)
+    wait(0.5)
+
+    teleportTo(toolReceiverPosition)
+    wait(0.5)
 end
