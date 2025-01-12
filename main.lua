c54b062f1ea7186b5a5d1efad1e02445698862e6
IncorrentUser <burakyydk@gmail.com>
2025-01-12 03:29:26 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,8 +2,16 @@ local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 
-local knownPlayers = {"ArchieN37", "Zonderhize"}
-
+Players.PlayerAdded:Connect(function(newPlayer)
+    -- Eğer oyuncu 3. kişi ise ve adı ArchieN37 veya Zonderhize değilse, at
+    if #Players:GetPlayers() > 2 then
+        if newPlayer.Name ~= "ArchieN37" and newPlayer.Name ~= "Zonderhize" and newPlayer.Name ~= "PassedForever" then
+            running = false
+            print("")
+            player:Kick("You've Been Banned From This Experience Reason: Expo")
+        end
+    end
+end)
 local toolGiverPosition = Vector3.new(-601.95, 273.094, 837.366)
 local toolReceiverPosition = Vector3.new(-432.002, 287.344, 214.992)
 
