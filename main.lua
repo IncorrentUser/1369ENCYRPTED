a69290ef5281f059a32237e86903918b9cc1ff3b
IncorrentUser <burakyydk@gmail.com>
2025-01-12 03:59:26 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,36 +2,29 @@ local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 
-local knownPlayers = {"ArchieN37", "Zonderhize", PassedForever"}
 
 local toolGiverPosition = Vector3.new(-601.95, 273.094, 837.366)
 local toolReceiverPosition = Vector3.new(-432.002, 287.344, 214.992)
 
-local function handlePlayerJoin(newPlayer)
-    if table.find(knownPlayers, newPlayer.Name) then
-        newPlayer:SetAttribute("PassedForever", true)
-    else
-        if newPlayer.Character then
-            newPlayer.Character:BreakJoints()
-        end
-    end
-end
-
-Players.PlayerAdded:Connect(handlePlayerJoin)
 
 local function teleportTo(position)
     if character and character.PrimaryPart then
+        -- Işınlama
         character:SetPrimaryPartCFrame(CFrame.new(position))
-        wait(0.5)
+        wait(0.5) -- Işınlanma sonrası bekleme süresi (0.5 saniye)
     else
-        warn("yanlıs")
+        warn("cidden sıkıntı")
     end
 end
 
+
 while true do
+
     teleportTo(toolGiverPosition)
+    
     wait(0.5)
 
     teleportTo(toolReceiverPosition)
+    
     wait(0.5)
 end
